26bc02da84c009143794490890526e763e3d68d1
DanielSleczka <danielsleczkads@gmail.com>
2022-04-15 13:01:07 +0200

    MarcoPolo systems created

---
+++ Assets/Scripts/MarcoPolo.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MarcoPolo : MonoBehaviour
+{
+    [SerializeField] private Button marcoPoloButton;
+    [SerializeField] private TextMeshProUGUI textMarcoPolo;
+
+
+    private void Start()
+    {
+        marcoPoloButton.onClick.AddListener(RunMarcoPolo);
+    }
+
+    public void RunMarcoPolo()
+    {
+        for (int i = 1; i <= 100; i++)
+        {
+            if (i % 3 == 0 && i % 5 == 0)
+            {
+                textMarcoPolo.text += "Marco Polo ";
+            }
+            else if (i % 3 == 0)
+            {
+                textMarcoPolo.text += "Marco ";
+            }
+            else if (i % 5 == 0)
+            {
+                textMarcoPolo.text += "Polo ";
+            }
+            else
+            {
+                textMarcoPolo.text += $"{i} ";
+            }
+        }
+    }
+
+
+}
